ce3e16b27e540173a8036a1505171b5ebb5641a0
bittoMazza <robertomazza99@gmail.com>
2022-07-04 14:28:09 +0200

    completed

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const app = new Vue(
         el:'#root',
         data: {
             textTitle : "",
+            newImageLink : "",
+            imageLink : "",
+
+        },
+        methods: {
+            changeImage(){
+                this.imageLink = this.newImageLink
+            }
         }
     }
 )
